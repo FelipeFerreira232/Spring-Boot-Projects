f99b8d8b9141f68b74b73d84c0d376cf3765dcbe
FelipeFerreira232 <144627019+FelipeFerreira232@users.noreply.github.com>
2024-03-02 14:32:22 -0300

    Add files via upload

---
+++ aspectsAOPexample/src/main/java/com/example3/aspects/aspectsAOPexample/AspectsAoPexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example3.aspects.aspectsAOPexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectsAoPexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectsAoPexampleApplication.class, args);
+	}
+
+}
---
+++ aspectsAOPexample/src/test/java/com/example3/aspects/aspectsAOPexample/AspectsAoPexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example3.aspects.aspectsAOPexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectsAoPexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
